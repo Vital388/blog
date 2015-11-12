23b4f51a6a05462da651ad1cc3b7b7ea25fbeb8b
v.fito <vitalzff@gmail.com>
2015-11-12 18:20:09 +0200

    save

--- routes/posts.js
+++ routes/posts.js
@@ -4,14 +4,14 @@ var posts_model = require('../model/db');
 /* GET users listing. */
 router.get('/', function (req, res, next) {
     var category = req.query.category;
-    if(category){
-        posts_model.find({category:category}).sort('-date').exec(function (err, posts) {
+    if (category) {
+        posts_model.find({category: category}).sort('-date').exec(function (err, posts) {
             res.render('posts', {posts: posts});
         })
-    }else{
-    posts_model.find().sort('-date').exec(function (err, posts) {
-        res.render('posts', {posts: posts});
-    })
+    } else {
+        posts_model.find().sort('-date').exec(function (err, posts) {
+            res.render('posts', {posts: posts});
+        })
     }
 });
 router.get('/create', function (req, res, next) {
@@ -30,7 +30,7 @@ router.get('/:postId', function (req, res, next) {
 
     var postId = req.params.postId;
     posts_model.find({_id: postId}, function (err, post) {
-        if (post) {
+        if (post.length > 0) {
             res.render('post', {post: post});
         } else {
             next();
@@ -43,7 +43,12 @@ router.post('/', function (req, res, next) {
 
     var post = req.body;
 
-    var posts = new posts_model({title: post.title, body: post.body, excerption: post.excerption,category:post.category});
+    var posts = new posts_model({
+        title: post.title,
+        body: post.body,
+        excerption: post.excerption,
+        category: post.category
+    });
     posts.save(function () {
         res.send({id: posts.id});
     })
