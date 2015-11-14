7ef05957b9c87d6d8136b46bc4e58c859898152d
v.fito <vitalzff@gmail.com>
2015-11-14 13:31:02 +0200

    add category jade

--- routes/posts.js
+++ routes/posts.js
@@ -3,15 +3,74 @@ var router = express.Router();
 var posts_model = require('../model/db');
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-    var category = req.query.category;
-    if (category) {
-        posts_model.find({category: category}).sort('-date').exec(function (err, posts) {
-            res.render('posts', {posts: posts});
-        })
+    /*var query, skipFrom, sort, columns, populate, lean, model = this;
+    columns = options.columns || null;
+    sort = options.sort || options.sortBy || null; // todo: get rid of sortBy in major version
+    populate = options.populate || null;
+    var lean = options.lean || false;
+    var pageNumber = options.page || 1;
+    var resultsPerPage = options.limit || 10;
+    skipFrom = (pageNumber * resultsPerPage) - resultsPerPage;
+    query = model.find(q);
+    if (columns !== null) {
+        query = query.select(options.columns);
+    }
+    query = query.skip(skipFrom).limit(resultsPerPage);
+    if (sort !== null) {
+        query.sort(sort);
+    }
+    if (populate) {
+        if (Array.isArray(populate)) {
+            populate.forEach(function(field) {
+                query = query.populate(field);
+            });
+        } else {
+            query = query.populate(populate);
+        }
+    }
+    if (lean) {
+        query.lean();
+    }
+
+    if (typeof callback === 'function') {
+        return handleCallback(query, q, model, resultsPerPage, callback);
     } else {
-        posts_model.find().sort('-date').exec(function (err, posts) {
-            res.render('posts', {posts: posts});
+        return handlePromise(query, q, model, resultsPerPage);
+    }
+}
+
+*/
+    var pages_count;
+    var pageNumber = req.query.page || 1;
+    var resultsPerPage = req.query.resultsPerPage||5;
+    var skipFrom = (pageNumber * resultsPerPage) - resultsPerPage;
+    posts_model.count({},function(err,c){
+        pages_count=c/resultsPerPage;
+        console.log(pages_count);
+        posts_model.find().skip(skipFrom).limit(resultsPerPage).sort('-date').exec(function (err, posts) {
+            res.render('posts', {posts: posts,pages_count:pages_count});
         })
+    });
+
+
+});
+router.get('/category/:catname', function (req, res, next) {
+    var pages_count;
+    var catname = req.params.catname;
+    var pageNumber = req.query.page || 1;
+    var resultsPerPage = req.query.resultsPerPage||2;
+    var skipFrom = (pageNumber * resultsPerPage) - resultsPerPage;
+    console.log(catname.length);
+    if(catname.length>0){
+    posts_model.count({category:catname},function(err,c){
+        pages_count=c/resultsPerPage;
+        console.log(pages_count);
+        posts_model.find({category:catname}).skip(skipFrom).limit(resultsPerPage).sort('-date').exec(function (err, posts) {
+            res.render('category', {posts: posts,pages_count:pages_count});
+        })
+    });
+    }else{
+      res.send(404);
     }
 });
 router.get('/create', function (req, res, next) {
@@ -29,6 +88,7 @@ router.get('/create', function (req, res, next) {
 router.get('/:postId', function (req, res, next) {
 
     var postId = req.params.postId;
+    if(postId.length>0){
     posts_model.find({_id: postId}, function (err, post) {
         if (post.length > 0) {
             res.render('post', {post: post});
@@ -36,6 +96,7 @@ router.get('/:postId', function (req, res, next) {
             next();
         }
     })
+    }
 });
 
 
